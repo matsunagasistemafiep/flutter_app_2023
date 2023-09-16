5fd21ef1b510f9cbb37984c36428c16506a1a207
Fabio Matsunaga <fabio.matsunaga@sesisenaipr.org.br>
2023-09-15 22:02:44 -0300

    lista de tarefas

--- lib/pages/lista_tarefas.dart
+++ lib/pages/lista_tarefas.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ListaTarefas extends StatefulWidget {
+  const ListaTarefas({super.key});
+
   @override
   State<ListaTarefas> createState() {
     return ListaTarefasState();
@@ -8,11 +11,77 @@ class ListaTarefas extends StatefulWidget {
 }
 
 class ListaTarefasState extends State<ListaTarefas> {
+
+  TextEditingController descriptionControl = TextEditingController();
+  TextEditingController dataControl = TextEditingController();
+  TextEditingController horaControl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Minhas tarefas")),
-      body: Container()
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: descriptionControl,
+              decoration: const InputDecoration(
+                labelText: "Descrição",
+                icon: Icon(Icons.description)
+              )
+            ),
+            TextFormField(
+              controller: dataControl,
+              readOnly: true,
+              decoration: const InputDecoration(
+                labelText: "Data da tarefa",
+                icon: Icon(Icons.calendar_month)
+              ),
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2022),
+                  lastDate: DateTime(2030)
+                );
+                if (pickedDate!=null) {
+                  String dataFormatada = DateFormat('dd/MM/yyyy').format(pickedDate);
+                  setState(() {
+                    dataControl.text = dataFormatada;
+                  });
+                }
+              }
+            ),
+            TextFormField(
+              controller: horaControl,
+              readOnly: true,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.av_timer),
+                labelText: "Hora da tarefa"
+              ),
+              onTap: () async {
+                TimeOfDay? pickedTime = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now()
+                );
+                if (pickedTime!=null) {
+                  setState(() {
+                    horaControl.text = "${pickedTime.hour}:${pickedTime.minute}";
+                  });
+                }
+              },
+            ),
+            ElevatedButton(
+              child: const Text("Cadastrar"),
+              onPressed: () {
+
+              },
+            ),
+            const Divider(),
+            
+          ]
+        ))
     );
   }
 }
